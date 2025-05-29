8975eee0e44399352abac58a556472420697e0a6
yuvasrir12 <yuvasrireddy6@gmail.com>
2025-05-30 00:24:48 +0530

    Create CalculatorReg .java

---
+++ Regression_Testing/CalculatorReg .java
@@ -0,0 +1,22 @@
+package regresspack;
+
+public class CalculatorReg {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
+    }
+}
